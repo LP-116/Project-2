38fe31065e2ccadb17059c371ba0cb78621d7f73
Leanne <leanne_porter@hotmail.com>
2021-09-06 15:38:05 +1000

    updating code

---
+++ app.py
@@ -0,0 +1,74 @@
+import datetime as dt 
+import numpy as np 
+import pandas as pd 
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func, inspect
+
+from flask import Flask, jsonify, render_template, redirect
+
+
+
+engine = create_engine("postgresql://postgres:bootcamp@localhost:5432/crime")
+
+
+# reflect an existing database into a new model
+Base = automap_base()
+
+# reflect the tables
+Base.prepare(engine, reflect=True)
+
+location = Base.classes.location
+crime = Base.classes.crime
+
+
+app = Flask(__name__)
+
+@app.route("/")
+def welcome():
+    return render_template("index.html")
+
+
+
+
+@app.route("/api/v1.0/all_data")
+def all_data():
+
+    session = Session(engine)
+    conn = engine.connect()
+
+    results = pd.read_sql("SELECT crime.*, location.latitude, location.longitude FROM crime JOIN location ON crime.suburb = location.suburb WHERE crime.year > 2020", conn)
+
+
+    return results.to_json(orient='split', index=False)
+
+
+@app.route("/api/v1.0/suburb_lga")
+def suburb_lga():
+
+    session = Session(engine)
+    
+    suburb_data = session.query(location.suburb, location.lga, location.latitude, location.longitude).all()
+
+    session.close()
+
+    all_suburbs = []
+
+    for suburb, lga, lat, lng in suburb_data:
+        suburb_dict = {}
+        suburb_dict["suburb"] = suburb
+        suburb_dict["lga"] = lga
+        suburb_dict["lat"] = lat
+        suburb_dict["lng"] = lng
+        all_suburbs.append(suburb_dict)
+
+
+    return jsonify(all_suburbs)
+
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
