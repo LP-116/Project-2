92dbd8806c491769a70f1cf568c4722005b368dc
Leanne <leanne_porter@hotmail.com>
2021-09-07 15:57:33 +1000

    updating data page

--- static/js/data-app.js
+++ static/js/data-app.js
@@ -18,3 +18,78 @@ function init() {
 };
 
 init()
+
+var tbody = d3.select("tbody");
+
+function buildTable() {
+
+    d3.json("/api/v1.0/data_tab").then((data) => {
+
+    data.forEach((dataRow) => {
+    
+        var row = tbody.append("tr");
+
+        Object.values(dataRow).forEach((value) => {
+            row.append("td").text(value);
+        });
+
+    });
+
+});
+}
+
+var filterButton = d3.select("#filter-btn");
+
+filterButton.on("click", runFilter);
+
+
+
+function runFilter() {
+
+    d3.event.preventDefault();
+
+    var suburbElement = d3.select("#suburbSelect");
+    
+    var suburbValue = suburbElement.property("value");
+    console.log(suburbValue);
+
+    var yearElement = d3.select("#yearSelect");
+    var yearValue = yearElement.property("value");
+    console.log(yearValue);
+
+    d3.json("/api/v1.0/data_tab").then((data)=> {
+        
+        var filteredData = data.filter(dataEntry => ((dataEntry[1] === suburbValue) && dataEntry[0] === parseInt(yearValue, 10)))
+
+        console.log(filteredData);
+
+        if (!filteredData.length) {
+
+            console.log("No result")
+    
+            tbody.html("");
+    
+            tbody.text("No results for selected inputs.");}
+    
+        else {
+        
+        tbody.html("");
+    
+        filteredData.forEach((dataEntry) => {
+    
+            var row = tbody.append("tr");
+    
+            Object.values(dataEntry).forEach((value) => {
+                row.append("td").text(value);
+            });
+        });
+
+    }
+
+});
+
+}
+
+
+
+
