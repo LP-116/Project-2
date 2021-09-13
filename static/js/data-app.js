513fa535f3a3540ce2fde4c45b89e42307d4c118
Leanne <leanne_porter@hotmail.com>
2021-09-13 11:27:49 +1000

    updating comments

--- static/js/data-app.js
+++ static/js/data-app.js
@@ -1,3 +1,5 @@
+// Function run on webpage load.
+// This first function populates the dropdown menu with the list of suburbs.
 function init() {
 
     d3.json("/api/v1.0/suburbs").then((item) => {
@@ -5,7 +7,6 @@ function init() {
         var dropdownMenu = d3.select("#myList");
 
         var dropdownNames = item;
-        // console.log(dropdownNames);
         
         dropdownNames.forEach((item) => {
         dropdownMenu
@@ -19,27 +20,10 @@ function init() {
 
 init()
 
-var tbody = d3.select("tbody");
 
-function buildTable() {
-
-    d3.json("/api/v1.0/data_tab").then((data) => {
-
-    data.forEach((dataRow) => {
-    
-        var row = tbody.append("tr");
-
-        Object.values(dataRow).forEach((value) => {
-            row.append("td").text(value);
-        });
-
-    });
-
-});
-}
-
+// Functions used to make the filter table button appear as loading.
 function startSpinner() {
-    // your code to make the spinner start
+    // Codde to make the spinner start
     $("#filter-btn").prop("disabled", true);
     $("#filter-btn").html(
         `<span class="spinner-border spinner-border-sm" role="status" aria-hidden="true"></span>&nbsp Loading...`
@@ -47,39 +31,49 @@ function startSpinner() {
 }
 
 function stopSpinner() {
-    // you code to make the spinner stop
+    // Code to make the spinner stop
     // (i.e., return the button to its original state)
     $("#filter-btn").prop("disabled", false);
     $("#filter-btn").html('Filter Table');
 }
 
 
+// Selecting the filter button.
 var filterButton = d3.select("#filter-btn");
 
+// On click of the filter button run the runfilter function.
 filterButton.on("click", runFilter);
 
 
+// Funtion to return table results based on the 
 function runFilter() {
 
+    // Once the button is clicked run the start spinner function.
     startSpinner()
 
+    // Prevent automatic reload.
     d3.event.preventDefault();
 
+    // Select the suburb in the dropdown box.
     var suburbElement = d3.select("#suburbSelect");
     
     var suburbValue = suburbElement.property("value");
-    console.log(suburbValue);
+    console.log(suburbValue);   
 
+    // Select the year in the dropdown box.
     var yearElement = d3.select("#yearSelect");
     var yearValue = yearElement.property("value");
     console.log(yearValue);
-
+    
+    // Using the data_tab route.
     d3.json("/api/v1.0/data_tab").then((data)=> {
         
+        // Filter the results on suburb and year.
         var filteredData = data.filter(dataEntry => ((dataEntry[1] === suburbValue) && dataEntry[0] === parseInt(yearValue, 10)))
 
         console.log(filteredData);
 
+        //  If there is no results, return text stating "no results for selected inputs".
         if (!filteredData.length) {
 
             console.log("No result")
@@ -89,6 +83,7 @@ function runFilter() {
             tbody.text("No results for selected inputs.");
             stopSpinner()}
     
+        // Else, populate the table with the filtereddata results.
         else {
         
         tbody.html("");
@@ -102,7 +97,7 @@ function runFilter() {
             });
         });
 
-    
+        // Stop the spinner once retrieval is complete.
         stopSpinner()  
 
     }
